981cb324e0db550eba7301fceff7b799275becf4
Bharathi Shavakkar <shavakkar@gmail.com>
2023-09-09 11:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ window.onscroll = () => {
 // ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer!', 'App Developer!'],
+    strings: ['Frontend Developer!', 'Php Developer!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
